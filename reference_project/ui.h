0d7a6aaefa54e94fa9f86b81bcbc976ff02de831
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-06-08 00:05:44 +0200

    use LV_LVGL_H_INCLUDE_SIMPLE to include lvgl.h

--- reference_project/ui.h
+++ reference_project/ui.h
@@ -14,7 +14,12 @@ extern "C" {
  *********************/
 
 /* Include all the UI libraries */
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
 #include "lvgl/lvgl.h"
+#endif
+
 #include "reference_project.h"
 
 /*********************
