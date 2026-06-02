47541d65b10635b4068a8ab320b417852e3f77e4
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2026-06-02 22:42:37 +0200

    more fixes

--- scripts/build_html_examples.sh
+++ scripts/build_html_examples.sh
@@ -2,53 +2,41 @@
 
 set -e
 
-# These variables allow us to specify an alternate repository URL and commit reference
-# This is particularly useful when running in CI environments for pull requests
-# where we need to build from the contributor's forked repository
-ARG_1="${1:-}"
-ARG_2="${2:-}"
-
 STARTER_PATH=`pwd`
+PROJ_PATH=`pwd`/lvgl_editor
 
 export PATH="/usr/lib/ccache:/usr/local/opt/ccache/libexec:$PATH"
-rm -rf emscripten_builder
+
+echo Clone the Emscripten port
+echo -------------------------
+
 
 if true; then
-git clone https://github.com/lvgl/lv_sim_emscripten.git emscripten_builder
-cd emscripten_builder
-if [ "$ARG_1" != "--symlink" ]; then
-  REPO_URL="$ARG_1"
-  COMMIT_REF="$ARG_2"
-  git submodule update --init --single-branch -- lvgl
-  if [ -n "$REPO_URL" ] && [ -n "$COMMIT_REF" ]; then
-    cd lvgl
-    echo "Using provided repo URL: $REPO_URL and commit ref: $COMMIT_REF for lvgl submodule"
-    git remote set-url origin "$REPO_URL"
-    git fetch origin
-    git checkout "$COMMIT_REF"
-  else
-    echo "Using the latest LVGL commit from master"
+    rm -rf emscripten_builder
+    git clone https://github.com/lvgl/lv_sim_emscripten.git emscripten_builder
+    cd emscripten_builder
+    git submodule update --init --single-branch -- lvgl
     cd lvgl
     git pull origin master
-  fi
-else
-  SYMLINK_TARGET="$ARG_2"
-  echo "Using provided symbolic link to LVGL directory (should be absolute): $SYMLINK_TARGET"
-  rmdir lvgl # remove the uninitialized submodule empty dir
-  ln -s -T "$SYMLINK_TARGET" lvgl
-fi
 fi
 
-cd $STARTER_PATH #Back to where lvgl_editor and emscripten_builder is
-
-LVGL_PATH=$STARTER_PATH/emscripten_builder/lvgl
+EMSCRIPTEN_BUILDER_PATH=$STARTER_PATH/emscripten_builder
+LVGL_PATH=$EMSCRIPTEN_BUILDER_PATH/lvgl
+echo EMSCRIPTEN_BUILDER_PATH: $EMSCRIPTEN_BUILDER_PATH
 echo LVGL_PATH: $LVGL_PATH
 
-# Grab the path to emscripten's port examplelist.c before changing to LVGL's directory
-EXAMPLE_LIST_C=$(pwd)/examplelist.c
-cd $STARTER_PATH
-./lvgl_editor/scripts/genexamplelist.sh > $EXAMPLE_LIST_C
-cd ..
+echo Generate example list
+echo ---------------------
+
+# Replace the lvgl examples with the lvgl_editor exmamples so that emscripten will build them
+rm -r $LVGL_PATH/examples
+cp -r $PROJ_PATH/examples $LVGL_PATH
+EXAMPLE_LIST_C=$EMSCRIPTEN_BUILDER_PATH/examplelist.c
+$STARTER_PATH/lvgl_editor/scripts/genexamplelist.sh > $EXAMPLE_LIST_C
+cat $EXAMPLE_LIST_C
+
+echo Generate lv_conf
+echo ----------------
 
 # Generate lv_conf
 LV_CONF_PATH=$LVGL_PATH/configs/ci/docs/lv_conf_docs.h
@@ -57,15 +45,17 @@ python $LVGL_PATH/scripts/generate_lv_conf.py \
   --config $LV_CONF_PATH \
   --defaults $LVGL_PATH/configs/ci/docs/lv_conf_docs.defaults
 
+echo Build the examples
+echo ------------------
 
+cd $EMSCRIPTEN_BUILDER_PATH
 emcmake cmake -B cmbuild -GNinja -DLV_BUILD_CONF_PATH=$LV_CONF_PATH -DLVGL_CHOSEN_DEMO=lv_example_noop -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
 cmake --build cmbuild
 
-rm -rf cmbuild/CMakeFiles
 
-if [ "$ARG_1" != "--symlink" ]; then
-  cp -a cmbuild ../docs/built_lv_examples
-else
-  cp -a cmbuild lvgl/docs/built_lv_examples
-fi
-cd ..
+echo Copy the built examples
+echo -----------------------
+
+
+rm -rf cmbuild/CMakeFiles
+cp -a cmbuild $STARTER_PATH/lvgl_editor/docs/built_lv_examples
